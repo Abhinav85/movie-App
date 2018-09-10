318c2be3f373d7b2d15c671d0c33a59a3a8cb126
abhinav85 <abhinav.rajput@upgrad.com>
2018-09-10 18:42:27 +0530

    First Commit

---
+++ src/common/header/Header.js
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './Header.css';
+import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg';
+import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import PropTypes from 'prop-types';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import BookShow from '../../screens/bookshow/BookShow'
+
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+}
+
+const TabContainer = function (props) {
+    return (
+        <Typography component="div" style={{ padding: 0, textAlign: 'center' }}>
+            {props.children}
+        </Typography>
+    );
+}
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+class Header extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            modalIsOpen: false,
+            value: 0,
+            username: "",
+            usernameRequired: "dispNone",
+            password: "",
+            passwordRequired: "dispNone"
+        };
+    }
+
+    openModalHandler = () => {
+        this.setState({ modalIsOpen: true })
+    }
+
+    closeModalHandler = () => {
+        this.setState({ modalIsOpen: false })
+    }
+    tabChangeHandler = (event, value) => {
+        this.setState({ value });
+    }
+
+    loginClickHandler = () => {
+        this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
+        this.state.password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
+
+    }
+    inputUsernameChangeHandler = (e) => {
+        this.setState({ username: e.target.value })
+    }
+
+    inputPasswordChangeHandler = (e) => {
+        this.setState({ password: e.target.value })
+    }
+
+    bookshowHandler = () => {
+        ReactDOM.render(<BookShow />, document.getElementById('root'));
+    }
+
+    render() {
+        return (
+            <div>
+                <header className="app-header">
+                    <img src={logo} className="app-logo" alt="logo" />
+                    <div className="login-button">
+                        <Button variant="contained" color="default" onClick={this.openModalHandler}>Login</Button>
+                    </div>
+                    {this.props.showBookShowButton === "true" ?
+                    <div className="bookshow-button">
+                        <Button variant="contained" color="primary" onClick={this.bookshowHandler}>
+                        BOOK SHOW</Button>
+                    </div>: ""}
+                </header>
+                <Modal
+                    ariaHideApp={false}
+                    isOpen={this.state.modalIsOpen}
+                    contentLabel="Login"
+                    onRequestClose={this.closeModalHandler}
+                    style={customStyles}
+                >
+                    <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login" />
+                        <Tab label="Register" />
+                    </Tabs>
+                    {this.state.value === 0 &&
+                        <TabContainer>
+                            <FormControl required>
+                                <InputLabel htmlFor="username"> Username </InputLabel>
+                                <Input id="username" type="text" username={this.state.username} onChange={this.inputUsernameChangeHandler} />
+                                <FormHelperText className={this.state.usernameRequired}><span className="red">required</span></FormHelperText>
+                            </FormControl><br /><br />
+                            <FormControl required>
+                                <InputLabel htmlFor="password"> Password </InputLabel>
+                                <Input id="password" type="password" onChange={this.inputPasswordChangeHandler} />
+                                <FormHelperText className={this.state.passwordRequired}><span className="red">required</span></FormHelperText>
+                            </FormControl><br /><br />
+                            <Button variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
+                        </TabContainer>}
+
+
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default Header;
---
+++ src/screens/confirmation/Confirmation.js
@@ -0,0 +1,176 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../../common/header/Header';
+import './Confirmation.css';
+import BookShow from '../../screens/bookshow/BookShow';
+import Home from '../../screens/home/Home';
+import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import green from '@material-ui/core/colors/green';
+
+
+const styles = theme => ({
+  close: {
+    width: theme.spacing.unit * 4,
+    height: theme.spacing.unit * 4,
+  },
+  success: {
+    color: green[600],
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  }
+});
+
+
+class Confirmation extends Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      open: false
+    }
+  }
+
+  handleArtistClick = (url) =>
+  {
+    window.location = url;
+  }
+
+  handleBackClick = () =>
+  {
+    ReactDOM.render(<BookShow id={this.props.id}  bookingSummary={this.props.bookingSummary} />, document.getElementById('root'));
+  }
+
+  handleConfirmBooking = () =>
+  {
+    this.setState({ open: true });
+  }
+
+  handleClose = () =>
+  {
+    ReactDOM.render(<Home />, document.getElementById('root'));
+  }
+
+
+  render() {
+    const { classes } = this.props;
+
+    return(
+      <div className="Details">
+        <Header />
+        <div className="confirmation marginTop16">
+            <div>
+              <Typography className="back" onClick={this.handleBackClick}>
+                  &#60; Back to Book Show
+              </Typography><br/>
+              <Card className="cardStyle">
+                <CardContent>
+                  <Typography  variant="headline" component="h2">
+                   SUMMARY
+                  </Typography><br/>
+
+                    <div className="coupon-container"><div className="confirmLeft"><Typography>Location:</Typography></div>
+                    <div><Typography> {this.props.bookingSummary.location}</Typography></div></div>
+                  <br/>
+
+                    <div className="coupon-container"><div className="confirmLeft"><Typography>Language:</Typography></div>
+                    <div><Typography> {this.props.bookingSummary.language}</Typography></div></div>
+                  <br/>
+
+                    <div className="coupon-container"><div className="confirmLeft"><Typography>Show Date:</Typography> </div>
+                    <div><Typography> {this.props.bookingSummary.showDate}</Typography></div></div>
+                  <br/>
+
+                    <div className="coupon-container"><div className="confirmLeft"><Typography>Show Time:</Typography></div>
+                    <div><Typography>  {this.props.bookingSummary.showTime}</Typography></div></div>
+                  <br/>
+
+                    <div className="coupon-container"><div className="confirmLeft"><Typography>Tickets:</Typography> </div>
+                    <div><Typography> {this.props.bookingSummary.tickets}</Typography></div></div>
+                  <br/>
+
+                    <div className="coupon-container"><div className="confirmLeft"><Typography>Unit Price:</Typography> </div>
+                    <div><Typography> {this.props.bookingSummary.unitPrice}</Typography></div></div>
+                  <br/>
+                  <div className="coupon-container">
+                    <div>
+                      <FormControl className="formControl">
+                        <InputLabel htmlFor="coupon">  <Typography>
+                            Coupon Code
+                          </Typography></InputLabel>
+                        <Input id="coupon" />
+                      </FormControl>
+                    </div>
+                    <div className="marginApply">
+                      <Button variant="contained" onClick={this.handleCouponApplyClick}  color="primary">
+                        Apply
+                      </Button>
+                    </div>
+                  </div>
+                  <br/><br/>
+
+                    <div className="coupon-container"><div className="confirmLeft"> <span className="bold">Total Price:</span></div>
+                    <div>  {this.props.bookingSummary.unitPrice * this.props.bookingSummary.tickets}</div></div>
+                   <br/>
+                  <Button variant="contained" onClick={this.handleConfirmBooking}  color="primary">
+                    Confirm Booking
+                  </Button>
+                </CardContent>
+              </Card>
+            </div>
+        </div>
+        <Snackbar
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            className="snackbar"
+            open={this.state.open}
+            onClose={this.handleClose}
+            message={
+              <span id="client-snackbar" className={classes.success}>
+                <div className="confirm"><div><CheckCircleIcon /></div><div className="msg"> Booking Confirmed!</div></div>
+              </span>
+            }
+            action={[
+              <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                className={classes.close}
+                onClick={this.handleClose}
+              >
+               <CloseIcon />
+              </IconButton>,
+            ]}
+          />
+      </div>
+
+    )}
+}
+
+Confirmation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Confirmation);
